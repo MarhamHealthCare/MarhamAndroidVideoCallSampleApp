8d61689043a8ec367b4a2f801d7fb552c4ad6f37
wazzah-marham <wazzah@marham.pk>
2022-12-09 19:55:40 +0500

    code after successfully integrating marham video call library

--- app/src/main/java/com/marham/marhamhealthlinetestapp/MainActivity.java
+++ app/src/main/java/com/marham/marhamhealthlinetestapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.marham.marhamhealthlinetestapp;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
@@ -14,21 +16,37 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.marham.marhamvideocalllibrary.MarhamVideoCallHelper;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String TAG = "Health Line Token: ";
 
+    private Button openMarhamAppButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpPushNotifications();
-        MarhamVideoCallHelper.initHelper(this);
+        setUpButton();
 
     }
 
-    private void setUpPushNotifications(){
+    private void setUpButton() {
+        openMarhamAppButton = findViewById(R.id.open_marham_app_button);
+        openMarhamAppButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.open_marham_app_button) {
+            MarhamVideoCallHelper.getInstance().setAPIKEY("").setFirebaseToken("").setPhoneNumber("").setName("");
+            MarhamVideoCallHelper.getInstance().launchBookingFlow(this);
+        }
+    }
+
+    private void setUpPushNotifications() {
         // Declare the launcher at the top of your Activity/Fragment:
         final ActivityResultLauncher<String> requestPermissionLauncher =
                 registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
@@ -55,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
 //                        String msg = getString(R.string.msg_token_fmt, token);
                         Log.e(TAG, token);
 //                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-                        Toast.makeText(MainActivity.this,token,Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
                     }
                 });
     }
