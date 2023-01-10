b162477446b2e117ca3da3cb2d15d903b7244e30
wazzah-marham <wazzah@marham.pk>
2023-01-10 10:42:24 +0500

    code after finalzing payments

--- app/src/main/java/com/marham/marhamhealthlinetestapp/MainActivity.java
+++ app/src/main/java/com/marham/marhamhealthlinetestapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.marham.marhamhealthlinetestapp;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +17,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.marham.marhamvideocalllibrary.MarhamVideoCallHelper;
+import com.marham.marhamvideocalllibrary.listeners.payment.MarhamPaymentListener;
+import com.marham.marhamvideocalllibrary.model.appointment.Appointment;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -41,10 +45,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         int id = view.getId();
         if (id == R.id.open_marham_app_button) {
-            MarhamVideoCallHelper.getInstance().setAPIKEY("").setFirebaseToken("").setPhoneNumber("").setName("");
+            MarhamVideoCallHelper.getInstance().setClient("telenor").setAPIKEY("$2y$10$UTa82jp.SycYYIc0wUn9r.2aqWEluFknWhki2Aooh3taGNsry3oA6").setFirebaseToken("fRHe-d--TkqGplJG_tgPJP:APA91bE9v2J6hop0mcaVx7wLYzT7n1iQRtTlNBaw-wxoEk9Wo4VvAlsnx0FJYYPEM8-q8CibWuo7Lpz4CczqalmSOKUsxS-mqQ4yDc4u-ZT5zg9fCuev56SYfRcq53Tt1tH94VHvHrhe").setUserPhoneNumber("+923334794867").setUserName("Wazzah").setMarhamPaymentListener(marhamPaymentListener);
             MarhamVideoCallHelper.getInstance().launchBookingFlow(this);
         }
     }
+
+    private MarhamPaymentListener marhamPaymentListener = new MarhamPaymentListener() {
+        @Override
+        public void onPaymentRequested(Appointment appointment) {
+            Toast.makeText(MainActivity.this, "Processing Payment ...", Toast.LENGTH_LONG).show();
+
+            final Handler handler = new Handler(Looper.getMainLooper());
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    MarhamVideoCallHelper.getInstance().openPaymentScreen(MainActivity.this, appointment);
+                }
+            }, 5000);
+
+        }
+    };
+
 
     private void setUpPushNotifications() {
         // Declare the launcher at the top of your Activity/Fragment:
