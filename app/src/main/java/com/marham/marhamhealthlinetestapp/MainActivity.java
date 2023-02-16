7bb48d98f314ca53e0d6029b2c8d2739a9809226
Hamza-ali <hamza.ali@marham.pk>
2023-02-16 16:04:25 +0500

    updated marhamAndroidVideoCall dependency from 0.0.23 to 0.0.24

--- app/src/main/java/com/marham/marhamhealthlinetestapp/MainActivity.java
+++ app/src/main/java/com/marham/marhamhealthlinetestapp/MainActivity.java
@@ -19,6 +19,7 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.marham.marhamvideocalllibrary.MarhamVideoCallHelper;
 import com.marham.marhamvideocalllibrary.listeners.payment.MarhamPaymentListener;
 import com.marham.marhamvideocalllibrary.model.appointment.Appointment;
+import com.marham.marhamvideocalllibrary.utils.MarhamVideoCallSharedPreference;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -31,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setUpPushNotifications();
+//        setUpPushNotifications();
         setUpButton();
 
     }
@@ -45,8 +46,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         int id = view.getId();
         if (id == R.id.open_marham_app_button) {
-            MarhamVideoCallHelper.getInstance().setClient("telenor").setAPIKEY("$2y$10$UTa82jp.SycYYIc0wUn9r.2aqWEluFknWhki2Aooh3taGNsry3oA6").setFirebaseToken("fRHe-d--TkqGplJG_tgPJP:APA91bE9v2J6hop0mcaVx7wLYzT7n1iQRtTlNBaw-wxoEk9Wo4VvAlsnx0FJYYPEM8-q8CibWuo7Lpz4CczqalmSOKUsxS-mqQ4yDc4u-ZT5zg9fCuev56SYfRcq53Tt1tH94VHvHrhe").setUserPhoneNumber("+923334794867").setUserName("Wazzah").setMarhamPaymentListener(marhamPaymentListener);
-            MarhamVideoCallHelper.getInstance().launchBookingFlow(this);
+
+//            Log.d(TAG, MarhamVideoCallSharedPreference.getInstance(this).getFCMToken());
+//            fRHe-d--TkqGplJG_tgPJP:APA91bE9v2J6hop0mcaVx7wLYzT7n1iQRtTlNBaw-wxoEk9Wo4VvAlsnx0FJYYPEM8-q8CibWuo7Lpz4CczqalmSOKUsxS-mqQ4yDc4u-ZT5zg9fCuev56SYfRcq53Tt1tH94VHvHrhe
+            MarhamVideoCallHelper.getInstance().setClient("telenor").setAPIKEY("$2y$10$UTa82jp.SycYYIc0wUn9r.2aqWEluFknWhki2Aooh3taGNsry3oA6").setFirebaseToken(MarhamVideoCallSharedPreference.getInstance(this).getFCMToken()).setUserPhoneNumber("+923334794867").setUserName("Wazzah").setMarhamPaymentListener(marhamPaymentListener);
+            MarhamVideoCallHelper.getInstance().openMarhamSdk(this);
         }
     }
 
